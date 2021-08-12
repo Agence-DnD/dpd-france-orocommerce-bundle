d5dd52fc696be90c15b19152c221a2dda3608a40
dnd-smash <dnd-smash@dnd.fr>
2021-08-12 13:23:36 +0200

    all: initial commit for DndDpdFranceShippingBundle

---
+++ DndDpdFranceShippingBundle.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AgenceDnd\DndDpdFranceShippingBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class DndDpdFranceShippingBundle
+ *
+ * @package   AgenceDnd\DndDpdFranceShippingBundle
+ * @author    Agence Dn'D <contact@dnd.fr>
+ * @copyright 2004-present Agence Dn'D
+ * @license   https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @link      https://www.dnd.fr/
+ */
+class DndDpdFranceShippingBundle extends Bundle
+{
+}
