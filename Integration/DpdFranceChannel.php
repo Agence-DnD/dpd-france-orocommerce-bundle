b63b7ac97f298af5b96b8986f21a40a24fea52b2
dnd-smash <dnd-smash@dnd.fr>
2021-08-16 09:16:55 +0200

    #4: WIP integrations initialization

---
+++ Integration/DpdFranceChannel.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dnd\Bundle\DpdFranceShippingBundle\Integration;
+
+use Oro\Bundle\IntegrationBundle\Provider\{ChannelInterface, IconAwareIntegrationInterface};
+
+/**
+ * Class DpdFranceChannel
+ *
+ * @package   Dnd\Bundle\DpdFranceShippingBundle\Integration
+ * @author    Agence Dn'D <contact@dnd.fr>
+ * @copyright 2004-present Agence Dn'D
+ * @license   https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @link      https://www.dnd.fr/
+ */
+class DpdFranceChannel implements ChannelInterface, IconAwareIntegrationInterface
+{
+    /**
+     * Description TYPE constant
+     *
+     * @var string TYPE
+     */
+    public const TYPE = 'dpd_france';
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return string
+     */
+    public function getLabel(): string
+    {
+        return 'dpd_france.channel.label';
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return string
+     */
+    public function getIcon(): string
+    {
+        return 'bundles/dpd_france/img/DPD_logo_icon.png';
+    }
+}
